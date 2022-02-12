ae3afcc9d671de1afc3d9425347413474a5ae8a6
Yash Vyas <vyasyas@hilti.com>
2022-02-12 15:26:25 +0530

    Initial Commit

---
+++ YVLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YVLearning
+//
+//  Created by Yash Vyas on 12/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YVLearning/YVLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  YVLearningApp.swift
+//  YVLearning
+//
+//  Created by Yash Vyas on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct YVLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
