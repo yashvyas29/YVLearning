d747ecf9f15550e01f128364180fef4470128aed
Yash Vyas <vyasyas@hilti.com>
2022-03-20 00:56:59 +0530

    Added async stream examples with useful function and traversing machanisms.

--- YVLearning/Playgrounds/StructuredConcurrency.playground/Contents.swift
+++ YVLearning/Playgrounds/StructuredConcurrency.playground/Contents.swift
@@ -1,7 +1,7 @@
 // To use app classes in playground
 import YVLearning
 // To use Task in playground
-//import _Concurrency
+import _Concurrency
 // To hide the unnecessary duplicate framework logs
 //import PlaygroundSupport
 //defer{ PlaygroundPage.current.finishExecution() }
@@ -16,3 +16,46 @@ Task {
 //    debugPrint("fetchImages async let: \(await imagesParallel?.count ?? 0)")
 }
  */
+
+func asyncIntSquareStream(forValues values: [Int]) -> AsyncStream<Int> {
+    var index = 0
+    return AsyncStream {
+        guard index < values.count else {
+            return nil
+        }
+
+        let value = values[index]
+        index += 1
+
+        let asyncSquareValueTask = Task {
+            value * value
+        }
+        return await asyncSquareValueTask.value
+    }
+}
+
+Task {
+    let values = [0, 1, 2, 3, 4, 5]
+    var index = 0
+    for await squareValue in asyncIntSquareStream(forValues: values) {
+        debugPrint("Square of \(values[index]) is \(squareValue)")
+        index += 1
+    }
+
+    var valueIterator = values.makeIterator()
+    let hasNonZeroValues = await asyncIntSquareStream(forValues: values).allSatisfy { $0 > 0 }
+    debugPrint("Are all values greater than zero? : \(hasNonZeroValues)")
+    if let minValue = await asyncIntSquareStream(forValues: values).min() {
+        debugPrint("Minimum value: \(minValue)")
+    }
+    if let maxValue = await asyncIntSquareStream(forValues: values).max() {
+        debugPrint("Maximum value: \(maxValue)")
+    }
+    let sum = await asyncIntSquareStream(forValues: values).reduce(0, +)
+    debugPrint("Sum of all the values: \(sum)")
+    var asyncIntSquareIterator = asyncIntSquareStream(forValues: values).makeAsyncIterator()
+    while let value = valueIterator.next(),
+          let squareValue = await asyncIntSquareIterator.next() {
+        debugPrint("Square of \(value) is \(squareValue)")
+    }
+}
